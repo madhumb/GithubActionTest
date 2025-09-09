22f5fac6d8b589a348f0159c4e086d79e1496fd2
Madhu bhilawadi <madhu@strivebenefits.com>
2025-09-09 10:32:23 +0530

    yml update

---
+++ lib/main_moon.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/foundation.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:sep_roject/main.dart';
+
+void main() async {
+  await runMainApp(firebaseOptions: DefaultFirebaseOptions.currentPlatform);
+}
+
+class DefaultFirebaseOptions {
+  static FirebaseOptions get currentPlatform {
+    switch (defaultTargetPlatform) {
+      case TargetPlatform.android:
+        return const FirebaseOptions(
+          apiKey: 'AIzaSyBmzGji5Gq6R5ydyLYVifYH3j8CYMY1eTM',
+          appId: '1:922586843536:ios:adc5925c6b755b74134b15',
+          messagingSenderId: '922586843536',
+          projectId: 'bookmark-my-benefits',
+          storageBucket: 'bookmark-my-benefits.firebasestorage.app',
+        );
+      case TargetPlatform.iOS:
+      case TargetPlatform.fuchsia:
+        throw UnsupportedError('Fuchsia not supported ');
+      default:
+        throw UnsupportedError('Unknown platform');
+    }
+  }
+}
