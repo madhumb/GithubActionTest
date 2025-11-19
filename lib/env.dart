784459e7a2e5a5be6f2dd7c540d95ba38c30d7a2
Madhu bhilawadi <madhu@strivebenefits.com>
2025-11-19 13:05:29 +0530

    added env

--- lib/env.dart
+++ lib/env.dart
@@ -4,8 +4,22 @@ part 'env.g.dart';
 
 @Envied(path: '.env')
 abstract class Env {
+  @EnviedField(varName: 'PROD_BASE_URL', obfuscate: true)
+  static final String prodBaseUrl = _Env.prodBaseUrl;
+  @EnviedField(varName: 'PROD_OLD_BASE_URL', obfuscate: true)
+  static final String prodOldBaseUrl = _Env.prodOldBaseUrl;
+  @EnviedField(varName: 'PROD_PUBLIC_LOG_EVENT', obfuscate: true)
+  static final String prodPublicLogEvent = _Env.prodPublicLogEvent;
+  @EnviedField(varName: 'SECRET_IV', obfuscate: true)
+  static final String secretIv = _Env.secretIv;
   @EnviedField(varName: 'SECRET_KEY', obfuscate: true)
   static final String secretKey = _Env.secretKey;
-  @EnviedField(varName: 'BASE_URL', obfuscate: true)
-  static final String baseURL = _Env.baseURL;
+  @EnviedField(varName: 'STAGE_BASE_URL', obfuscate: true)
+  static final String stageBaseUrl = _Env.stageBaseUrl;
+  @EnviedField(varName: 'STAGE_OLD_BASE_URL', obfuscate: true)
+  static final String stageOldBaseUrl = _Env.stageOldBaseUrl;
+  @EnviedField(varName: 'STAGE_PUBLIC_LOG_EVENT', obfuscate: true)
+  static final String stagePublicLogEvent = _Env.stagePublicLogEvent;
+  @EnviedField(varName: 'TENOR_API_KEY', obfuscate: true)
+  static final String tenorApikey = _Env.tenorApikey;
 }
